24ab99e01c23d83b5df1580626f1648c99010aea
Niek Janssen <niek.janssen01@gmail.com>
2018-03-04 17:22:07 +0100

    transfer commit

--- osd/main.py
+++ osd/main.py
@@ -1,7 +1,35 @@
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+
+
+def noop():
+    pass
+
+
+class PPColoredButton(Button):
+    color = 0
+
+    def getColor(self):
+        return [[9, .9, .9, 1],
+                [.6, .6, .6, 1],
+                [.3, .3, .3, 1]][self.color]
+
+    def click(self):
+        App.get_running_app().toolManager.selectColor(self.color)
+
+
+class PPImagedButton(Button):
+    icon = 'resources/brush.png'
+    tool = -1
+
+    def build(self):
+        pass
+
+    def click(self):
+        App.get_running_app().toolManager.selectTool(self.tool)
 
 
 class DrawSpace(Widget):
@@ -13,7 +41,34 @@ class Root(FloatLayout):
     pass
 
 
+class ToolManager:
+    print = 0
+    undo = 1
+    redo = 2
+    brush = 3
+    fill = 4
+    erase = 5
+    line = 6
+    circle = 7
+    square = 8
+    selectedTool = brush
+    selectedColor = 0
+
+    def selectTool(self, tool):
+        self.selectedTool = tool
+        print('tool selected: ' + str(tool))
+
+    def selectColor(self, color):
+        if self.selectedColor == color:
+            self.selectedColor = -1
+            print('color selected: -1')
+        else:
+            self.selectedColor = color
+            print('color selected: ' + str(color))
+
+
 class PannenkoekenApp(App):
+    toolManager = ToolManager()
 
     def build(self):
         self.title = "Pannenkoekenswag"
