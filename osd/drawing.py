7af620e023c25dbfe57e02407238dcdac19f1717
Niek Janssen <niek.janssen01@gmail.com>
2018-03-05 16:20:05 +0100

    multiple files

--- osd/drawing.py
+++ osd/drawing.py
@@ -1,3 +1,9 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+from osd.util import UndoStack
+
+
 class Tool:
     def __init__(self, color, addInstruction, canvas):
         self.color = color
@@ -22,6 +28,9 @@ class Brush(Tool):
         with self.canvas:
             self.line = Line(points=(touch.x, touch.y))
 
+    def move(self, touch):
+        self.line.points += (touch.x, touch.y)
+
 
 class Fill(Tool):
     pass
@@ -41,3 +50,53 @@ class Circle(Tool):
 
 class Square(Tool):
     pass
+
+
+class DrawSpace(Widget):
+    tools = [None, None, None,
+             Brush, Fill, Erase,
+             Line, Circle, Square]
+    tool = None
+    selectedTool = 3
+    selectedColor = 0
+    touchState = False
+
+    def __init__(self, **kwargs):
+        super(DrawSpace, self).__init__(**kwargs)
+        toolManager = App.get_running_app().toolManager
+        toolManager.drawingModel = self
+        self.stack = UndoStack(toolManager.undo, toolManager.redo)
+        self.replaceTool()
+
+    def selectTool(self, tool):
+        if tool == 1:
+            self.stack.undo()
+        elif tool == 2:
+            self.stack.redo()
+        else:
+            self.selectedTool = tool
+            self.replaceTool()
+
+    def selectColor(self, color):
+        self.selectedColor = color
+        self.replaceTool()
+
+    def replaceTool(self):
+        self.tool = self.tools[self.selectedTool](self.selectedColor, self.addInstruction, self.canvas)
+
+    def addInstruction(self, instruction):
+        self.stack.push(instruction)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            self.tool.press(touch)
+            self.touchState = True
+
+    def on_touch_move(self, touch):
+        if self.touchState:
+            self.tool.move(touch)
+
+    def on_touch_up(self, touch):
+        if self.touchState:
+            self.tool.up(touch)
+        self.touchState = False
