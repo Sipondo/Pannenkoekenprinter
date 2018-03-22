6ef946fe3403abf3a413fbbd9acb995d338f9186
Ties Robroek <tt_robroek@hotmail.com>
2018-03-22 13:57:58 +0100

    first test run

---
+++ Slicer/driver.py
@@ -0,0 +1,113 @@
+import atexit
+import math
+import threading
+from time import sleep
+
+import RPi.GPIO as GPIO
+from Adafruit_MotorHAT import Adafruit_MotorHAT
+
+mh = Adafruit_MotorHAT(addr=0x70)
+pos_x = 0
+pos_y = 0
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup([7], GPIO.OUT)
+
+
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+
+def calibrate(myStepper):
+    myStepper.setSpeed(20)
+    calibration_length = 0;
+    while (True):
+        print("Length: " + str(calibration_length))
+        if (raw_input("Continue?") == "N"):
+            break;
+        calibration_length = calibration_length + 200
+        myStepper.step(200, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+
+    while (True):
+        print("Length: " + str(calibration_length))
+        if (raw_input("Continue?") == "N"):
+            break;
+        calibration_length = calibration_length + 20
+        myStepper.step(20, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+
+    print("Final calibrated length: " + str(calibration_length))
+    return calibration_length
+
+
+atexit.register(turnOffMotors)
+
+myStepper1 = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1                 # 30 RPM
+myStepper2 = mh.getStepper(200, 2)  # 200 steps/rev, motor port #1            # 30 RPM
+
+
+def stepper_worker(myStepper, distance, speed):
+    myStepper.setSpeed(int(speed))
+    if (distance > 0):
+        myStepper.step(distance, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+    else:
+        if (distance < 0):
+            myStepper.step(abs(distance), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
+
+
+def move(x, y):
+    x = int(x/1)
+    y = int(y/-1)
+    # global pos_x, pos_y
+    # max_distance = math.sqrt(1.0 * x * x + y * y)
+    # pos_x = pos_x + x;
+    # pos_y = pos_y + y;
+
+    # if (x != 0):
+    #    st1 = threading.Thread(target=stepper_worker, args=(myStepper1, x, max(0, 30.0 * (abs(x)/max_distance))))
+    # if (y != 0):
+    #    st2 = threading.Thread(target=stepper_worker, args=(myStepper2, y, max(0, -3.0 + 24.0 * (abs(y)/max_distance))))
+
+    if (x != 0):
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, x, 20))
+    if (y != 0):
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, y, 20))
+
+    if (x != 0):
+        st1.start()
+    if (y != 0):
+        st2.start()
+
+    if (x != 0):
+        st1.join()
+    if (y != 0):
+        st2.join()
+
+#used_set = instruction_dictionary['kill_me']
+
+def move_vector(vector):
+    GPIO.cleanup(7)
+    sleep(.2)
+    for next_instruction in vector:
+        move(next_instruction[0], next_instruction[1])
+    print("beslag uit!")
+    GPIO.cleanup(7)
+    sleep(.2)
+    pass
+
+def print_vector(vector):
+    GPIO.cleanup(7)
+    sleep(.2)
+    init_pos = vector[0]
+    move(init_pos[0], init_pos[1])
+    sleep(.2)
+    print("beslag aan!")
+    GPIO.setup([7], GPIO.OUT)
+    GPIO.output(7, 1)
+    sleep(.2)
+    for next_instruction in vector[1:]:
+        move(next_instruction[0], next_instruction[1])
+    print("beslag uit!")
+    GPIO.cleanup(7)
+    sleep(.2)
--- slicer/slice.py
+++ slicer/slice.py
@@ -6,7 +6,11 @@
 IMAGE_SIZE=(256,256)
 
 ##################
+import os
 import sys
+
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
 #print(sys.path)
 #sys.path.append("Slicer\\")
 import slice_lib as slib
@@ -42,10 +46,10 @@ for imgname in image_names[1:2]:
     display_image, display_layered,\
      seg_bot, seg_mid, seg_top = slib.segment_image(pic_array, gaussian)
 
-    plt.imshow(display_image, cmap="YlOrBr_r")
-    plt.show()
-    plt.imshow(display_layered ** .2 + 1000, cmap="gist_ncar")
-    plt.show()
+    # plt.imshow(display_image, cmap="YlOrBr_r")
+    # plt.show()
+    # plt.imshow(display_layered ** .2 + 1000, cmap="gist_ncar")
+    # plt.show()
 
     # fig=plt.figure()
     # ax=fig.add_subplot(111,projection='3d')
@@ -59,13 +63,10 @@ np.insert(seg_mid[0][:,50][:-1],0,True).shape
 np.flip(np.argwhere(np.logical_xor(np.insert(seg_mid[0][:,50][:-1],0,False),seg_mid[0][:,50])),0)
 
 vectors = []
-for img in (seg_bot+seg_mid+seg_top):
-    plt.imshow(img, cmap="YlOrBr_r")
-    plt.show()
-    vectors.append(vectorize(img))
-
-len(vectors)
-len(vectors[0])
+# for img in (seg_bot+seg_mid+seg_top):
+#     plt.imshow(img, cmap="YlOrBr_r")
+#     plt.show()
+#     #vectors.append(vectorize(img))
 
 
 def vectorize(segment):
@@ -80,72 +81,79 @@ def vectorize(segment):
                 xor = np.flip(xor,0)
             for j in range(int(len(xor)/2)):
                 vector_list.append(((int(xor[2*j]),i),(int(xor[2*j+1]),i)))
-            going_left = !going_left
+            going_left = not going_left
     return vector_list
 
 
 def MultiContourSegment(segment):
     vector_list = []
 
-
-plt.imshow(seg_bot[0], cmap="YlOrBr_r")
-plt.show()
+#
+# plt.imshow(seg_bot[0], cmap="YlOrBr_r")
+# plt.show()
 
 
 from skimage import measure
 contours = measure.find_contours(seg_bot[0],.1)
 
-for n, contour in enumerate(contours):
-    plt.plot(contour[:, 1], 256-contour[:, 0], linewidth=5)
-    #print(n, contour)
-plt.xlim([0,256])
-plt.ylim([0,256])
-plt.show()
+contour = contours[1]
+#print(contour)
+plt.plot(contour[:, 1], 256-contour[:, 0], linewidth=5)
 
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+import driver as drv
+drv.print_vector(contour)
 
-
-#fig = Figure(figsize=(1.3+256.0/72.0,1.3+256.0/72.0))
-fig = Figure(figsize=(256.0/72.0,256.0/72.0), frameon=False)
-canvas = FigureCanvas(fig)
-ax = fig.gca() #fig.add_axes([0, 0, 256, 256])
-ax.axes.set_xlim(0,256)
-ax.axes.set_ylim(0,256)
-
-#ax.text(0.0,0.0,"Test", fontsize=45)
-#ax.plot()
-for n, contour in enumerate(contours):
-    ax.plot(contour[:, 1], 256-contour[:, 0], linewidth=5)
-
-
-#fig.patch.set_visible(False)
-ax.patch.set_visible(False)
-ax.axis('off')
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-ax.spines['left'].set_visible(False)
-canvas.draw()       # draw the canvas, cache the renderer
-
-
-
-width, height = fig.get_size_inches() * fig.get_dpi()
-
-image = np.fromstring(canvas.tostring_rgb(), dtype='uint8').reshape(int(height), int(width), 3)
-
-
-#image = np.fromstring(canvas.tostring_rgb(), dtype='uint8').reshape(height, width, 3)
-
-# (373248/3)**.5
-# 373248/610
-# 610*610
-offset = int((image.shape[0]-256)/2)
-
-
-# image = image[:372100].reshape((610,610))
-plt.imshow(seg_bot[0])
-#plt.imshow(image[offset:-offset,offset:-offset], alpha=0.6)
-plt.imshow(image, alpha=0.6)
-plt.show()
-pass
+# for n, contour in enumerate(contours):
+#     plt.plot(contour[:, 1], 256-contour[:, 0], linewidth=5)
+#     print(n)
+#     #print(n, contour)
+# plt.show()
+# plt.xlim([0,256])
+# plt.ylim([0,256])
+# plt.show()
+#
+# from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+# from matplotlib.figure import Figure
+#
+#
+# #fig = Figure(figsize=(1.3+256.0/72.0,1.3+256.0/72.0))
+# fig = Figure(figsize=(256.0/72.0,256.0/72.0), frameon=False)
+# canvas = FigureCanvas(fig)
+# ax = fig.gca() #fig.add_axes([0, 0, 256, 256])
+# ax.axes.set_xlim(0,256)
+# ax.axes.set_ylim(0,256)
+#
+# #ax.text(0.0,0.0,"Test", fontsize=45)
+# #ax.plot()
+# for n, contour in enumerate(contours):
+#     ax.plot(contour[:, 1], 256-contour[:, 0], linewidth=5)
+#
+# #fig.patch.set_visible(False)
+# ax.patch.set_visible(False)
+# ax.axis('off')
+# ax.spines['top'].set_visible(False)
+# ax.spines['right'].set_visible(False)
+# ax.spines['bottom'].set_visible(False)
+# ax.spines['left'].set_visible(False)
+# canvas.draw()       # draw the canvas, cache the renderer
+#
+#
+#
+# width, height = fig.get_size_inches() * fig.get_dpi()
+#
+# image = np.fromstring(canvas.tostring_rgb(), dtype='uint8').reshape(int(height), int(width), 3)
+#
+#
+# #image = np.fromstring(canvas.tostring_rgb(), dtype='uint8').reshape(height, width, 3)
+#
+# # (373248/3)**.5
+# # 373248/610
+# # 610*610
+# offset = int((image.shape[0]-256)/2)
+#
+#
+# # image = image[:372100].reshape((610,610))
+# plt.imshow(seg_bot[0])
+# #plt.imshow(image[offset:-offset,offset:-offset], alpha=0.6)
+# plt.imshow(image, alpha=0.6)
+# plt.show()
